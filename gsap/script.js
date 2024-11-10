c0b1976dc99676fb021f4501df8156caec929d72
CodeExplorer4267 <rupambhadra478@gmail.com>
2024-11-10 19:02:11 +0530

    second-commit

---
+++ gsap/script.js
@@ -0,0 +1,62 @@
+/*gsap.to('#box1',{
+    x:1000,
+    y:349,
+    rotate:360,
+    backgroundColor:'green',
+    scale:0.7,
+    duration:3,
+    delay:1,
+    borderRadius:'50%',
+})
+gsap.from('#box1',{
+    x:1000,
+    y:349,
+    rotate:360,
+    backgroundColor:'green',
+    scale:0.7,
+    duration:3,
+    delay:1,
+    borderRadius:'50%',
+})*/
+/*gsap.from('h1',{
+    y:150,
+    duration:2,
+    delay:1,
+    color:'yellow',
+    opacity:0,
+    stagger:0.4
+})*/
+/*gsap.to('#circle',{
+    x:1000,
+    borderRadius:'10px',
+    rotate:360,
+    repeat:-1,
+    yoyo:true,
+    duration:2,
+    delay:1
+})*/
+//gsap timeline
+/*
+var tl=gsap.timeline()
+tl.to('#box1',{
+    x:1000,
+    rotate:360,
+    duration:2,
+    delay:1,
+    yoyo:true,
+    repeat:-1
+})
+tl.to('#box2',{
+    x:1000,
+    rotate:360,
+    duration:2,
+    yoyo:true,
+    repeat:-1
+})
+tl.to('#box3',{
+    x:1000,
+    rotate:360,
+    duration:2,
+    yoyo:true,
+    repeat:-1
+})*/
